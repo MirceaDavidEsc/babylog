71360f7a6702b328a6e0a1d5104262cffd991088
Mircea Davidescu <mdavidescu@protonmail.com>
2024-09-13 16:11:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -35,14 +35,8 @@ def add_activity():
     with open('log.txt', 'a') as f:
         f.write(f"{time},{activity},{notes}\n")
 
-<<<<<<< HEAD
-    # Sort log entries by time in descending order (most recent first)
-    log_entries_sorted = sorted(log_entries, key=lambda x: datetime.strptime(x.split(',')[0], '%Y-%m-%dT%H:%M:%S'), reverse=True)
-    return jsonify(success=True, log_entries=log_entries_sorted)
-=======
     # Return success and the updated log
     get_log()
->>>>>>> parent of a6ffe48... Update app.cpython-39.pyc, app.py, log.txt, and 1 more file Made a "refresh log" button to reload text data. Added mobile friendly formatting to CSS
 
 if __name__ == '__main__':
     app.run()
